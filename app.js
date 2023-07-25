f8e79885741e829f1984170d1dc28a245c5978fc
srijai14 <134276707+srijai14@users.noreply.github.com>
2023-07-25 10:57:12 -0700

    add js

---
+++ app.js
@@ -0,0 +1,7 @@
+const form = document.getElementById("form").value;
+const username = document.getElementById("username").value;
+const email = document.getElementById("email").value;
+const password = document.getElementById("password").value;
+const cpassword = document.getElementById("cpassword").value;
+
+// https://www.freecodecamp.org/news/learn-javascript-form-validation-by-making-a-form/
